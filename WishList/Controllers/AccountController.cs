41c7460f274e028d6d46f053c96dccf443e47181
ChristopherPham <Christopher_pham@yahoo.com>
2022-04-12 22:35:00 -0500

    constructor crated for accounts controller.

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -11,9 +11,10 @@ namespace WishList.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager , SignInManager<ApplicationUser>)
+        public AccountController(UserManager<ApplicationUser> userManager , SignInManager<ApplicationUser> signInManager)
         {
-
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
