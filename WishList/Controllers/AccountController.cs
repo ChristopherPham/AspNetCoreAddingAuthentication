51b86f1b54b1b1152a5aed9f7ffcc37e29ee3db3
ChristopherPham <Christopher_pham@yahoo.com>
2022-04-12 22:33:07 -0500

    Create AccountController.  Create Private Fields For AccountController.  Create Constructor For AccountController

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WishList.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager , SignInManager<ApplicationUser>)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
